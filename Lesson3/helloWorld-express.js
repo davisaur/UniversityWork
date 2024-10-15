76d969e05ab33f78aa75df5dc80d13b349d48756
davi <39279817+davisaur@users.noreply.github.com>
2024-10-15 09:54:33 +0100

    express.js route handling

---
+++ Lesson3/helloWorld-express.js
@@ -0,0 +1,26 @@
+const express = require('express'); // Import the express module
+
+const app = express(); // Create an express application
+const port = 3000; // Set the port number
+
+// Define a route handler for the default home page
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+// Start the server on the specified port
+app.listen(port, () => {
+    console.log(`Running at http://localhost:${port}`);
+})
+
+// -- Taking in URL parameters --
+// GET http://example.com/api?id=123
+
+app.get('/api', (req, res) => {
+    res.send(`ID: ${req.query.id}`);
+}
+
+// POST https://example.com/api     { "id": 123, "val": "hello!" }
+app.post('/api', (req, res) => {
+    res.send(`ID: ${req.body.id}, VAL: ${req.body.val}`);
+}
